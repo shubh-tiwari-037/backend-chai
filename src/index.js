07b03262fbdfb6a6cfa3f1f7cd7dcebc12e42a30
shubh-tiwari-037 <shubhtiwari037@gmail.com>
2025-07-29 09:41:04 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -8,7 +8,14 @@ dotenv.config({
 
 
 connectDB()
-
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`server is listining on port ${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+console.log("DB CONNECTION FAILED !!!", error)
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+
+
+const asyncHandler= (requestsHandler)=>{
+    (req,res, next)=>{
+        Promise.resolve(requestsHandler(req,res,next))
+       .catch((err)=> next(err))
+    }
+}
+
+export {asyncHandler}
+
+
+
+// const asyncHandler=()=>{}
+// const asyncHandler= (func)=>  ()=> {}
+// const asyncHandler = (func)=> async()=>{}
+
+    // const asyncHandler =(fn) => async (req , res, next)=>{
+    //     try {
+    //         await fn(req, res, next)
+    //     } catch (error) {
+    //         res.status(error.code || 500).json({
+    //             success:false,
+    //             message:error.message
+    //         })
+    //     }
+    // }
