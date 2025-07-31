8f7ecca56ca77dc7c76c93cce3df0f212a56d9b3
shubh-tiwari-037 <shubhtiwari037@gmail.com>
2025-07-31 15:22:29 +0530

    fix in multer file

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,11 +1,11 @@
 import multer from "multer"
 
 const storage = multer.diskStorage({
-    destination: function (req,res,cb){
+    destination: function (req,file,cb){
         cb(null,"./public/temp")
     },
-    filename: function(req,res,cb){
-        cb(null,this.file.originalname)
+    filename: function(req,file,cb){
+        cb(null,file.originalname)
     }
 })
 
